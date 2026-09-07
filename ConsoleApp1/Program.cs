1a9ed68aa1597325f4fe00c64cfa9d2441f54786
EduJonas26 <jonas.ahling.net26@edu.edugrade.se>
2026-09-07 20:05:40 +0200

    addedPayment

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,6 +5,12 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            System.Console.WriteLine("Oskar was here");
+            System.Console.WriteLine("we dont like this part nono");
+       System.Console.WriteLine("asd");
+       System.Console.WriteLine("sd");
+       System.Console.WriteLine("bost");
+       
         }
     }
 }
